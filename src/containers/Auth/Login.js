90e41d6577f4bfcd314ae4f9d9c188a61e150053
Thanh02022002 <thanhvipad2002@gmail.com>
2024-05-02 19:25:04 +0700

    #38 4rd commit

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -6,6 +6,7 @@ import * as actions from "../../store/actions";
 
 import './Login.scss';
 import { FormattedMessage } from 'react-intl';
+import { handleLoginApi } from '../../services/userService';
 
 
 class Login extends Component {
@@ -15,6 +16,7 @@ class Login extends Component {
             username: '',
             password: '',
             isShowPassword: false,
+            errMassage: '',
         }
     }
 
@@ -31,13 +33,33 @@ class Login extends Component {
         })
     }
 
-    handleLogin = () => {
-        console.log('username: ', this.state.username, 'password: ', this.state.password)
-        console.log('all state ', this.state)
+    handleLogin = async () => {
+        this.setState({
+            errMassage: ''
+        })
 
+        try {
+            let data = await handleLoginApi(this.state.username, this.state.password);
+            if (data && data.errCode !== 0) {
+                this.setState({
+                    errMessage: data.message
+                })
+            }
+            if (data && data.errCode === 0) {
+                this.props.userLoginSuccess(data.user)
+            }
+        } catch (error) {
+            if (error.response) {
+                if (error.response.data) {
+                    this.setState({
+                        errMassage: error.response.data.message
+                    })
+                }
+            }
+        }
     }
 
-    handleShowHidePassword =() =>{
+    handleShowHidePassword = () => {
         this.setState({
             isShowPassword: !this.state.isShowPassword
         })
@@ -65,13 +87,16 @@ class Login extends Component {
                                     placeholder='Enter your password'
                                     onChange={(event) => { this.handOnChangePassword(event) }}
                                 />
-                                <span 
-                                onClick={() =>{this.handleShowHidePassword()}}>
-                                    <i class={this.state.isShowPassword ?'far fa-eye': 'far fa-eye-slash'}></i>
+                                <span
+                                    onClick={() => { this.handleShowHidePassword() }}>
+                                    <i class={this.state.isShowPassword ? 'far fa-eye' : 'far fa-eye-slash'}></i>
 
                                 </span>
                             </div>
 
+                        </div>
+                        <div className='col-12' style={{color: 'red'}}>
+                            {this.state.errMassage}
                         </div>
                         <div className='col-12'>
                             <button className='btn-login' onClick={() => { this.handleLogin() }}>Log in</button>
@@ -102,8 +127,8 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => {
     return {
         navigate: (path) => dispatch(push(path)),
-        adminLoginSuccess: (adminInfo) => dispatch(actions.adminLoginSuccess(adminInfo)),
-        adminLoginFail: () => dispatch(actions.adminLoginFail()),
+        //userLoginFail: () => dispatch(actions.adminLoginFail()),
+        userLoginSuccess: (userInfor) => dispatch(actions.userLoginSuccess(userInfor))
     };
 };
 
